50e0ba2a70f853c10d71586abc8e1656d9171d1e
Gabriel Age <0800.grc@gmail.com>
2017-10-08 18:03:19 -0300

    Changing docker build shell for CD

--- docker-build.sh
+++ docker-build.sh
@@ -1,11 +1,11 @@
 # docker build shell to easy the Jenkins CI process
-actual_version="1.5.0"
-old_version="1.1.0"
+actual_version="1.0.1"
+old_version="1.0.0"
 
 #balance options
-balance_port1="3010"
-balance_port2="3011"
-balance_port3="3012"
+balance_port1="4011"
+balance_port2="4012"
+balance_port3="4013"
 
 #building new image
 echo Building container to v$actual_version
@@ -23,9 +23,9 @@ docker container rm colorbucket-api-3-v$old_version
 docker image rm colorbucket-api:$old_version
 
 #starting new containers
-docker run --name colorbucket-api-1-v$actual_version -p $balance_port1:3010 -d colorbucket-api:$actual_version
-docker run --name colorbucket-api-2-v$actual_version -p $balance_port2:3010 -d colorbucket-api:$actual_version
-docker run --name colorbucket-api-3-v$actual_version -p $balance_port3:3010 -d colorbucket-api:$actual_version
+docker run --name colorbucket-api-1-v$actual_version -p $balance_port1:4010 -d colorbucket-api:$actual_version
+docker run --name colorbucket-api-2-v$actual_version -p $balance_port2:4010 -d colorbucket-api:$actual_version
+docker run --name colorbucket-api-3-v$actual_version -p $balance_port3:4010 -d colorbucket-api:$actual_version
 
 #debug
 #echo Press any key to stop
